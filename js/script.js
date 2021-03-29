8747f993256d80fafecd69ece39852c05245f5c4
chreey1076 <81352898+chreey1076@users.noreply.github.com>
2021-03-29 17:19:30 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+﻿$(function(){
+	/*팝업창*/
+	$(".logo").click(function(){
+		$("#Pdjqckd").show();
+	});
+	$(".end").click(function(){
+		$("#Pdjqckd").hide();
+	});
+
+	
+	/*로그인창*/
+	$(".block").mouseover(function(){
+		$("fieldset").show();		
+	});
+	$(".block").mouseout(function(){
+		$("fieldset").hide();		
+	});
+	
+	
+	/*슬라이더*/
+	
+	$("figure img").each(function(aa){
+		$(this).css({left:aa*1200});
+	});
+
+	setInterval(function(){
+		$("figure img").each(function(){
+			var left=parseInt($(this).css("left"));
+			var move=left-1200;
+			$(this).animate({left:move},1000,function(){
+				if(left==-1200){
+					$(this).css({left:1200});
+				}
+			});//ani 끝
+		});//each 끝
+	},3000);//set 끝
+	
+	/*메뉴*/
+	
+	$("#menu1 .MEun1").mouseover(function(){
+		$(this).find(".all").stop().slideDown();
+	});
+	$("#menu1 .MEun1").mouseout(function(){
+		$(this).find(".all").stop().slideUp();
+	});
+});
